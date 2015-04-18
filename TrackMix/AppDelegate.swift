9f6acce3df7eb933ccfeac44ebb7bd87e1500091
Tomochika Hara <tomochikahara@gmail.com>
2015-04-18 15:02:15 +0900

    Initial Commit

---
+++ TrackMix/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TrackMix
+//
+//  Created by Tomochika Hara on 2015/04/18.
+//  Copyright (c) 2015年 Tomochika Hara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TrackMix/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrackMix
+//
+//  Created by Tomochika Hara on 2015/04/18.
+//  Copyright (c) 2015年 Tomochika Hara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
